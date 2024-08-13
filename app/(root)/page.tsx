e9397a9677eb6614eb21a25004489417d8e47081
ZanQuen <andereyan@gmail.com>
2024-08-13 13:43:27 +0800

    Making Sidebar (Progress 18%)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-    const loggedIn = { firstName: "Andereyan" };
+    const loggedIn = { firstName: "Andereyan", lastName: "Muhammat", email: "andereyan@gmail.com" };
     return (
         <section className="home">
             <div className="home-content">
@@ -19,7 +20,13 @@ const Home = () => {
                         totalCurrentBalance={1000.43}
                     />
                 </header>
+                Recent Transaction
             </div>
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{}, {}]}
+            />
         </section>
     );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = ({ user }: SidebarProps) => {
     return (
         <section className="sidebar">
             <nav className="flex flex-col gap-4">
-                <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+                <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
                     <Image
                         src="/icons/logo.svg"
                         width={34}
@@ -26,11 +26,21 @@ const Sidebar = ({ user }: SidebarProps) => {
                     const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
                     return (
                         <Link href={item.route} key={item.label} className={cn('sidebar-link', { 'bg-bankGradient': isActive })}>
-                            {item.label}
+                            <div className="relative size-6">
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({ 'brightness-[3] invert-0': isActive })}
+                                />
+                            </div>
+                            <p className={cn('sidebar-label', { '!text-white': isActive })}>{item.label}</p>
                         </Link>
                     )
                 })}
+                User
             </nav>
+            Footer
         </section>
     )
 }
